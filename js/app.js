8c5e9bf1335228e9c4fbf13fdd8b8c8b94d5adca
Artur Shelashskiy <ashelashskiy@ronasit.com>
2016-02-14 23:55:40 +0600

    react-router

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 import React from  'react';
-import ReactDOM from 'react-dom';
+import History from 'history';
+import {render} from 'react-dom';
+import { Router, Route, Link, browserHistory } from 'react-router';
 import Main from './components/Main';
+import LocationDetails from './components/LocationDetails';
 import Normalize from '../css/normalize.css';
 
-ReactDOM.render(<Main />, document.getElementById('react'));
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={Main} />
+  </Router>
+), document.getElementById('react'))
---
+++ js/components/LocationDetails.js
@@ -0,0 +1,9 @@
+import React, { PropTypes } from 'react'
+
+class LocationDetails extends React.Component {
+    render () {
+        <div>Xdd</div>
+    }
+}
+
+export default LocationDetails;
