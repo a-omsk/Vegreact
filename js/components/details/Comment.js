3faa487648e6197b792c866650e462ee996231d5
Artur Shelashskiy <ashelashskiy@ronasit.com>
2016-02-17 17:52:11 +0600

    leave comment component

--- js/components/details/Comment.js
+++ js/components/details/Comment.js
@@ -5,7 +5,7 @@ import Rater from 'react-rater';
 const Comment = (props) => {
     return (
         <li style={{background: 'none'}} className='list-group-item'>
-            <h3 className='list-author'>Joe Hill</h3>
+            <h4 className='list-author'>Joe Hill</h4>
             <div className='list-date'>{moment(props.date).format("YYYY MMMM DD HH:mm")}</div>
             <Rater style={{pointerEvents: 'none'}} total={props.rating} rating={props.rating} />
             <div className='list-body'>{props.text}</div>
--- js/components/details/CommentForm.js
+++ js/components/details/CommentForm.js
@@ -1,9 +1,45 @@
 import React, { PropTypes } from 'react'
 
-const CommentForm = (props) => {
-    return (
-        <div>CommentForm</div>
-    )
+class CommentForm extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            visible: false
+        }
+
+        this.toggleVisibility = () => {
+            this.setState({visible: !this.state.visible});
+        }
+    }
+
+    render () {
+        let content;
+
+        if (this.state.visible) {
+            content = (
+                <form>
+                    <div className="form-group">
+                        <input className='comment-author' type="text" />
+                    </div>
+                    <div className="form-group">
+                        <div className='comment-rating'></div>
+                    </div>
+                    <div className="form-group">
+                        <textarea className='comment-body-area' />
+                    </div>
+                    <button className='btn-default btn'>Отправить</button>
+                </form>)
+        } else {
+            content = (
+                <button className='btn-default btn' onClick={this.toggleVisibility.bind(this)}>
+                    Оставить комментарий
+                </button>)
+        }
+
+        return <div>{content}</div>;
+    }
 }
 
 export default CommentForm;
